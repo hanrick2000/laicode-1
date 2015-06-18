06571d49b8ee27b083b484116fa8f818d112af89
Xu Tian <tianxu08@gmail.com>
2015-06-18 12:17:18 -0700

    add binary tree traversal

--- src/lab/TreeTraversal.java
+++ src/lab/TreeTraversal.java
@@ -3,6 +3,7 @@ package lab;
 import java.util.LinkedList;
 import java.util.Stack;
 
+import ds.ListNode;
 import ds.TreeNode;
 
 public class TreeTraversal {
@@ -24,17 +25,94 @@ public class TreeTraversal {
 			return;
 		}
 		LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
-		stack.offer(root);
+		stack.offerFirst(root);
 		while(!stack.isEmpty()) {
 			// visit the node
-			TreeNode cur = stack.poll();
+			TreeNode cur = stack.pollFirst();
 			System.out.print(cur.val + " ");
-			if () {
-				
+			if (cur.right != null) {
+				stack.offerFirst(cur.right);
+			}
+			if (cur.left != null) {
+				stack.offerFirst(cur.left);
 			}
 		}
 	}
 	
+	
+	// inorder
+	/*
+	 * The problem is, we can not throw away the root in the stack before we traversed 
+	 * all the nodes in left subtree. 
+	 * How can we know we have already traversed all the nodes in left sub?
+	 * Use a helper node to store the current “visiting” node to help identify 
+	 * when we can go back to the previous level.
+	 * 1 when helper node is not null, push helper and let helper = helper.left
+	 * 2 when helper is null, means the left subtree is finished, we can print the top, 
+	 * and let helper = top.right. (traverse the left subtree first, then root, then right subtree)
+	 * 3 do 1 and 2 until helper is null and there is no nodes left in the stack.
+	 */
+	public static void inOrder(TreeNode root) {
+		if (root == null) {
+			return ;
+		}
+		LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+		TreeNode cur = root;
+		while(cur != null || !stack.isEmpty()) {
+			if (cur != null) {
+				// push cur into stack
+				stack.offerFirst(cur);
+				// go left
+				cur = cur.left;
+			} else {
+				// cur == null
+				cur = stack.pollFirst();
+				// visit the node
+				System.out.print(cur.val + " ");
+				// go to cur.right
+				cur = cur.right;
+			}
+		}
+	}
+	
+	
+	
+	
+	// post Order Traversal
+	/*
+	 * left subtree, right subtree, root --> reverse order of root, right subtree, left subtree
+	 * 
+	 */
+	public static void postOrder1(TreeNode root) {
+		if (root == null) {
+			return ;
+		}
+		LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+		LinkedList<TreeNode> stack2 = new LinkedList<TreeNode>();
+		stack.offerFirst(root);
+		while(!stack.isEmpty()) {
+			TreeNode cur = stack.pollFirst();
+			stack2.offerFirst(cur);
+			if (cur.left != null) {
+				stack.offerFirst(cur.left);
+			}
+			if (cur.right != null) {
+				stack.offerFirst(cur.right);
+			}
+		}
+		
+		while(!stack2.isEmpty()) {
+			System.out.print(stack2.pollFirst().val + " ");
+		}
+	}
+	
+	
+	public static void postOrder2(TreeNode root) {
+		if (root == null) {
+			return ;
+		}
+		ListNode prev = null;
+	}
 	
 	
 	
