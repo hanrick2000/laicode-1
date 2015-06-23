46f307d1ae868741ebb1f719c57091ca49cd1545
tianxu08 <tianxu08@gmail.com>
2015-06-23 16:31:01 -0700

    Update Class6_Arrays3.java

--- src/small_yan/Class6_Arrays3.java
+++ src/small_yan/Class6_Arrays3.java
@@ -279,6 +279,7 @@ public class Class6_Arrays3 {
 	 * merge
 	 * find the consecutive element. if all the three elements are in different, calculate the  |x­-y|+ |y­-z| + |z­-x|.
 	 * 
+	 *!!!!! this doesn't work well. Wrong idea
 	 */
 	public static class Item8 {
 		public int val;
