a6cbe4af2d919317c693b7a30831416669f4cbcb
Xu Tian <tianxu08@gmail.com>
2015-12-09 09:46:45 -0800

    small yan class1

--- src/small_yan/Class1_BinarySearch_LinkedList.java
+++ src/small_yan/Class1_BinarySearch_LinkedList.java
@@ -1,10 +1,16 @@
 package small_yan;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Class1_BinarySearch_LinkedList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//		test1();
+//		test2_1();
+//		test2_2_2();
+		test3();
 	}
 	
 	
@@ -66,6 +72,13 @@ public class Class1_BinarySearch_LinkedList {
 	 *  return a[left] < a[right]? left : right;
 	 *
 	 */
+	public static void test1() {
+		int[] array = {2,1,3};
+		int localMin = task1_localMinPos(array);
+		int localMinVal = array[localMin];
+		System.out.println("localMin = " + localMin);
+		System.out.println("localMinVal = " + localMinVal);
+	}
 	
 	public static int task1_localMinPos(int[] array) {
 		if (array ==null || array.length == 0) {
@@ -101,7 +114,8 @@ public class Class1_BinarySearch_LinkedList {
 		while(left + 1 < right) {
 			int mid = left + (right - left)/2;
 			if (array[mid] < array[right]) {
-				// rotated part in the left side, the minimum should also in the left side
+				// rotated part in the left side, 
+				// the minimum should also in the left side
 				right = mid;
 			} else {
 				// in the right side
@@ -121,6 +135,12 @@ public class Class1_BinarySearch_LinkedList {
 	 * array[mid] compare to array[right] right­­
 	 */
 	
+	public static void test2_1() {
+		int[] array = {3,3,3,3,3,1,3};
+		int minPos = task2_minPosition_Rotated_WithDup(array);
+		System.out.println("minPos = " + minPos);
+		System.out.println("minPosVal = " + array[minPos]);
+	}
 	
 	public static int task2_minPosition_Rotated_WithDup(int[] array) {
 		int left = 0, right = array.length - 1;
@@ -141,27 +161,39 @@ public class Class1_BinarySearch_LinkedList {
 	
 	/*
 	 * task2.2.1
-	 * Given sorted int array, “rotated then reversed” first increasing. find the maximum value.
+	 * Given sorted int array, “rotated then reversed” first increasing. 
+	 * find the maximum value.
 	 * 1, 2, 3, 4, 5, 6 → 5, 6, 1, 2, 3, 4 → 5, 6, 4, 3, 2 ,1
 	 * 
 	 * 5, 6, 4, 3, 2, 1
 	 * 
 	 * array[mid] > array[left] : left = mid; ==> right part  
 	 * array[mid] < array[left] : right = mid; ==> left part
+	 * 
 	 */
 	
 	/*
 	 * task2.2.2
 	 * !!!this is slightly different with 2.2.1. 
-	 * although the right part is reversed, it still smaller than than the left part. 
+	 * although the right part is reversed, 
+	 * it still smaller than than the left part. 
 	 * However, 2.2.2 doesn't have this restriction. 
 	 * 
-	 * Given sorted int array, first increasing then decreasing. find the maximum value.
+	 * Given sorted int array, first increasing then decreasing.
+	 * find the maximum value.
 	 * 
 	 * {1, 3, 7, 6, 4, 2, 0}
-	 * mid ­ 1 mid mid + 1 decreasing right part , right = mid
-	 * mid ­ 1 mid mid + 1 increasing left part left = mid;
+	 * mid ­- 1,  mid, mid + 1 decreasing right part , right = mid
+	 * mid ­- 1,  mid, mid + 1 increasing left part left = mid;
 	 */
+	public static void test2_2_2() {
+		int[] array = {1,3,5,7,6,4,2,0};
+		int maxIndex = task2_2_2_maxValue(array);
+		System.out.println("maxIndex = " + maxIndex);
+		System.out.println("maxVal = " + array[maxIndex]);
+		
+	}
+	
 	public static int task2_2_2_maxValue(int[] array) {
 		int left = 0, right = array.length - 1;
 		while(left < right - 1) {
@@ -194,24 +226,46 @@ public class Class1_BinarySearch_LinkedList {
 	
 	// using swap to its index
 	// the input array is unsorted
-	/* index: 0 1 2
-	 * input: 3 2 4
-	 * 
-	 *  
+	/* index:   0   1   2
+	 * input:   3   2   4
+	 * i:       0
+	 * a[i]-1:  2
+	 * modify:  4   2   3
+	 * i:           1
+	 * a[i]-1       1
+	 *  	0   1   2   3   4
+	 *  	1   5   4   2   6
+	 *      1   6           5
+	 *      1   6   2   4   5
+	 *      1   2   6   4   5
+	 *       
 	 */
+	
+	public static void test3() {
+		int[] array = {1,5,4,2,6};
+		int rev = task3_missing(array);
+		System.out.println("val = " + rev);
+	}
 	public static int task3_missing(int[] array) {
 		if (array == null || array.length == 0) {
 			return 1;
 		}
+		System.out.println(Arrays.toString(array));
 		// swap with its index
 		for(int i = 0;i < array.length;) {
-			if (array[i] != i + 1 && array[i] <= array.length) {
-				// 
+			// array[i] - 1 is the index which it belongs to  
+			// if index != i  the element is not in its right position
+			// index is within bound
+			if (array[i] - 1 != i && array[i] - 1 < array.length) {
+				// swap the element to its position
 				swap(array, i, array[i] - 1);
 			} else {
 				i ++;
 			}
+			System.out.println(Arrays.toString(array));
 		}
+		System.out.println("debug--------");
+		System.out.println(Arrays.toString(array));
 		// find the result
 		for(int i = 0; i < array.length; i ++) {
 			if (array[i] != i + 1) {
