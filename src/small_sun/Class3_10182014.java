c368cee4d03090381d0dd3cfe287e2b840255c47
Xu Tian <tianxu08@gmail.com>
2015-12-18 09:30:48 -0800

    1218

--- src/small_sun/Class3_10182014.java
+++ src/small_sun/Class3_10182014.java
@@ -1,5 +1,7 @@
 package small_sun;
 
+import ds.TreeNode;
+
 public class Class3_10182014 {
 
 	public static void main(String[] args) {
@@ -17,6 +19,62 @@ public class Class3_10182014 {
 	 * 2 get depth from LCA of a and b, if level_a == level_b, return True
 	 *   otherwise, return false;
 	 */
+	public static boolean task1_isCousins(TreeNode root, TreeNode n1, TreeNode n2) {
+		if (root == null || root == n1 || root == n2) {
+			return false;
+		}
+		
+		TreeNode LCA = task1_getLCA(root, n1, n2);
+		if (LCA == n1 || LCA == n2 || isChild(LCA, n1) || isChild(LCA, n2)) {
+			return false;
+		}
+		int n1_level = getLevel(root, n1, 0);
+		int n2_level = getLevel(root, n2, 0);
+		if (n1_level == n2_level) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean isChild(TreeNode p, TreeNode c) {
+		boolean rev = false;
+		if (p.left == c || p.right == c) {
+			rev = true;
+		}
+		return rev;
+	}
+	
+	
+	
+	public static TreeNode task1_getLCA(TreeNode root, TreeNode n1, TreeNode n2) {
+		if (root == null) {
+			return root;
+		}
+		if (root == n1 || root == n2) {
+			return root;
+		}
+		TreeNode left = task1_getLCA(root.left, n1, n2);
+		TreeNode right = task1_getLCA(root.right, n1, n2);
+		if (left != null && right != null) {
+			return root;
+		}
+		return left != null ? left : right;
+	}
+	
+	public static int getLevel(TreeNode root, TreeNode n, int level) {
+		if (root == null) {
+			return 0;
+		}
+		if (root == n) {
+			return level;
+		}
+		int nextLevel = getLevel(root.left, n, level + 1);
+		if (nextLevel != 0) {
+			return nextLevel;
+		}
+		nextLevel = getLevel(root.right, n, nextLevel);
+		return nextLevel;
+	}
 	
 	
 	/*
@@ -38,6 +96,8 @@ public class Class3_10182014 {
 	 * (2)two direction, find the k closest elements
 	 * 
 	 */
+	
+	
 	
 	
 
--- src/small_sun/Class4_10252014.java
+++ src/small_sun/Class4_10252014.java
@@ -1,18 +1,70 @@
 package small_sun;
 
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+import ds.TreeNode;
+
 public class Class4_10252014 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//		test1();
+		test2();
+		
 	}
 	/*
 	 * Q1   Given a Binary Tree, print Right view of it. 
 	 * Right view of a Binary Tree is set of nodes visible when tree is visited from Right side.
 	 * 
 	 * (1) level order
-	 * (2) like the preOrder traverse recursion. 
+	 * (2) like the preOrder traverse recursion.
+	 * 
+	 * set a golbal variable, maxLevel, 
+	 * root, root.right, root.left
+	 * every time, if level > maxLevel, it's the first time to visit this level, print the right most node's value
+	 *  
 	 */
+	public static void test1() {
+		TreeNode n1 = new TreeNode(1);
+		TreeNode n2 = new TreeNode(2);
+		TreeNode n3 = new TreeNode(3);
+		TreeNode n4 = new TreeNode(4);
+		TreeNode n5 = new TreeNode(5);
+		n1.left = n2;
+		n1.right = n3;
+		n3.left = n4;
+		n4.left = n5;
+		
+		task1_printRightView(n1);
+	}
+	public static class MaxLevel {
+		public int value;
+		public MaxLevel(int v) {
+			this.value = v;
+		}
+	}
+	
+	public static void task1_printRightView(TreeNode root) {
+		if (root == null) {
+			return ;
+		}
+		MaxLevel maxLevel = new MaxLevel(-1);
+		task1_helper(root, 0, maxLevel);
+	}
+	
+	public static void task1_helper(TreeNode node,int level,  MaxLevel maxLevel) {
+		if (node == null) {
+			return ;
+		}
+		if (level > maxLevel.value) {
+			// print out
+			System.out.print(node.val + " ");
+			maxLevel.value = level;
+		}
+		task1_helper(node.right, level + 1, maxLevel);
+		task1_helper(node.left, level + 1, maxLevel);
+	}
 	
 	
 	/*
@@ -20,7 +72,67 @@ public class Class4_10252014 {
 	 * The following example illustrates vertical order traversal.
 	 * 
 	 * implement
+	 * 
+	 * Use TreeMap
+	 * 
+	 *                  1
+	 *                /   \
+	 *               2     3
+	 *              / \   / \
+	 *             4   5 6   7
+	 * 
+	 * Time: traverse the tree O(n). create the tree and every time access key in treeMap O(log n)
+	 * So, the total time is : O(n * log n)
+	 *    
 	 */
+	public static void test2() {
+		TreeNode n1 = new TreeNode(1);
+		TreeNode n2 = new TreeNode(2);
+		TreeNode n3 = new TreeNode(3);
+		TreeNode n4 = new TreeNode(4);
+		TreeNode n5 = new TreeNode(5);
+		TreeNode n6 = new TreeNode(6);
+		TreeNode n7 = new TreeNode(7);
+		
+		n1.left = n2;
+		n1.right = n3;
+		n2.left = n4;
+		n2.right = n5;
+		n3.left = n6; 
+		n3.right = n7;
+		
+		task2_print_vertically(n1);
+	}
+	
+	public static void task2_print_vertically(TreeNode root) {
+		TreeMap<Integer, ArrayList<Integer>> map = new TreeMap<Integer, ArrayList<Integer>>();
+		task2_getMap(root, map, 0);
+		
+		// print out
+		for(int i = map.firstKey(); i <= map.lastKey(); i ++) {
+			System.out.println(map.get(i));
+		}
+	}
+	
+	public static void task2_getMap(TreeNode node, TreeMap<Integer, ArrayList<Integer>>map, int hdist) {
+		if (node == null) {
+			return ;
+		}
+		if (map.containsKey(hdist)) {
+			ArrayList<Integer> list = map.get(hdist);
+			list.add(node.val);
+		} else {
+			ArrayList<Integer> list = new ArrayList<Integer>();
+			list.add(node.val);
+			map.put(hdist, list);
+		}
+		task2_getMap(node.left, map, hdist - 1);
+		task2_getMap(node.right, map, hdist + 1);
+	}
+	
+	
+	
+	
 	
 	/*
 	 * Q3:   设有N堆沙子排成一排,其编号为1,2,3,...,N(N<=100)。每堆沙子有一定的数量。
