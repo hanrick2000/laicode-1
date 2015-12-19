b46e794a42493a070f51e70bec75c38ade43e583
Xu Tian <tianxu08@gmail.com>
2015-12-18 23:39:47 -0800

    1218

--- src/small_sun/Class5_11022014.java
+++ src/small_sun/Class5_11022014.java
@@ -1,15 +1,80 @@
 package small_sun;
 
+import java.util.Arrays;
+
 public class Class5_11022014 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test1();
+	}
+	
 	/*
 	 * Q1. Given a list of records, each record has (start, end, weight). 
 	 * find a subset of the records, such that there is no overlap, and the sum of weight is maximized.
 	 * 
+	 * state[x] : the maximum weights ending at records whose starting at x
+	 * state[x] = weight[x] + max(state[i]) if no overlapping between x and i  (0 <= i < x)
+	 * 
 	 */
+	public static void test1() {
+		Record r1 = new Record(0, 2, 3);
+		Record r2 = new Record(1, 2, 4);
+		Record r3 = new Record(3, 5, 9);
+		Record[] records = {r1, r2, r3};
+		int rev = task1_max_sum_weight(records);
+		System.out.println(rev);
+	}
+	
+	
+	public static class Record{
+		int x;
+		int y;
+		int w;
+		public Record(int _x, int _y, int _w) {
+			this.x = _x;
+			this.y = _y;
+			this.w = _w;
+		}
+	}
+	public static int task1_max_sum_weight(Record[] records) {
+		int n = records.length;
+		int[] state = new int[n];
+		state[0] = records[0].w;
+		for(int i = 1; i < n; i ++) {
+			int prevMax = 0;
+			for(int j = 0; j < i ; j ++) {
+				if (records[i].x < records[j].y) {
+					// there is overlap
+					continue;
+				} else {
+					// update the maximum previous weight
+					prevMax = Math.max(prevMax, state[j]);
+				}
+			}
+			// update the state
+			state[i] = prevMax + records[i].w;
+		}
+		System.out.println(Arrays.toString(state));
+		
+		int maxSumWeight = Integer.MIN_VALUE;
+		for(int i = 0; i < n; i ++) {
+			maxSumWeight = Math.max(state[i], maxSumWeight);
+		}
+		
+		return maxSumWeight;
+	}
+	
+	
 	
 	/*
 	 * Q2   Given two binary trees (with unique letters), check if the first tree is subtree of the second one. 
 	 * A subtree of a tree T is a tree S consisting of a node in T and all of its descendants in T.
+	 * 
+	 * API
+	 * isSameTree(TreeNode root1, TreeNode root2);
+	 * 
+	 * Traverse the second tree, find the node which equals first root;
+	 * call isSameTree(node, root1)
 	 */
 	
 	/*
