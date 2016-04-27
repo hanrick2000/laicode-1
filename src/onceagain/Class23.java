109061eecfc7eb32a97e4caf98912a41cdee8dc7
Xu Tian <tianxu08@gmail.com>
2016-04-26 17:53:33 -0700

    04226016

---
+++ src/onceagain/Class23.java
@@ -0,0 +1,53 @@
+package onceagain;
+
+public class Class23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/*
+	 * task1
+	 * Determine if Binary Tree Is Balanced
+	 */
+	
+	/*
+	 * task2
+	 * Max Path Sum Binary Tree II(path from any node to any node)
+	 */
+	
+	/*
+	 * task3
+	 * Max Path Sum Binary Tree(path from leaf to root)
+	 */
+	
+	/*
+	 * task4
+	 * Binary Tree Path Sum To Target
+	 * (the two nodes can be the same node and they can only be on the path from root to one of the leaf nodes)
+	 */
+	
+	/*
+	 * task5
+	 * Max Path Sum Binary Tree III
+	 * (the two nodes can be the same node and they can only be on the path from root to one of the leaf nodes)
+	 */
+	
+	/*
+	 * task6
+	 * Common Elements In Three Sorted Arrays
+	 */
+	
+	/*
+	 * task7
+	 *  Longest Common Substring
+	 */
+	
+	/*
+	 * task8
+	 * Longest Common Subsequence
+	 */
+	
+	
+}
---
+++ src/onceagain/Class24.java
@@ -0,0 +1,15 @@
+package onceagain;
+
+public class Class24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/*
+	 * task1
+	 * 
+	 */
+
+}
